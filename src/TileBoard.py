bf5ae2b9d4829f78d0fba03a63293ae696b9aa43
devjoshdev <jsinanan1@gmail.com>
2023-02-24 00:07:31 -0500

    Set up skeleton of the TileBoard class

---
+++ src/TileBoard.py
@@ -0,0 +1,21 @@
+class TileBoard:
+
+    ## TODO: need to validate the board passed before instantiating
+    def __init__(self, arr):
+        pass
+    
+    ## TODO: return true or false depending on success (or throw error?)
+    def move(self, direction):
+        pass
+    
+    def is_goal(self):
+        pass
+
+    @staticmethod
+    def create_random_board():
+        pass
+
+    @staticmethod
+    def create_from_file(file_path):
+        pass
+
